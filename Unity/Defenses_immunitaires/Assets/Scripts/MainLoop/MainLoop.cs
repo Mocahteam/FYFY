eb6f81d50a58949dcdc68c92b31a2e9fc74596d2
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-04-14 12:00:35 +0000

    When duplicate Main_Loop automatic GO, delete the MainLoop component && Add feedback when Systems list updated during runtime.

--- Unity/Defenses_immunitaires/Assets/Scripts/MainLoop/MainLoop.cs
+++ Unity/Defenses_immunitaires/Assets/Scripts/MainLoop/MainLoop.cs
@@ -5,6 +5,12 @@ using UnityEditor;
 [AddComponentMenu("")] // hide in Component list
 public class MainLoop : MonoBehaviour {
 	public MonoScript[] _systemFiles; // = {}; // -> to avoid runtime add problem if mainLoopMenu active during playing mode
+	// variable to know if main kept editor value, so it is initialized ? => avoid to add component main in script | Probleme si cest plus en editor mode car elle sera jamais setted ??
+
+	#if UNITY_EDITOR
+	[SerializeField]
+	private bool _systemFilesChanged;
+	#endif
 
 	private UECS.System[] _systems;
 	private int _systemsLength;
@@ -18,6 +24,10 @@ public class MainLoop : MonoBehaviour {
 			if(systemFile != null)
 				_systems[_systemsLength++] = (UECS.System) System.Activator.CreateInstance(systemFile.GetClass());
 		}
+
+		#if UNITY_EDITOR
+		_systemFilesChanged = false;
+		#endif
 	}
 
 	private void Start(){
@@ -25,6 +35,13 @@ public class MainLoop : MonoBehaviour {
 	}
 
 	private void FixedUpdate(){
+		#if UNITY_EDITOR
+		if (_systemFilesChanged == true) {
+			// Family f = FamilyManager.getFamily(new AllOfTypes(typeof(Transform)));
+			_systemFilesChanged = false;
+		}
+		#endif
+
 		int count = UECS.EntityManager._delayedActions.Count;
 		while(count-- > 0)
 			UECS.EntityManager._delayedActions.Dequeue().perform();
@@ -34,10 +51,6 @@ public class MainLoop : MonoBehaviour {
 	}
 }
 
-// PROBLEME CONSTRUCTOR UNITY MULTIPLE APPEL ?? regarder serialisation etc
-
-// priority ? Order ? Change order ?
-
 // GameObject hiddenMainLoop = new GameObject("Hidden_Main_Loop");
 // hiddenMainLoop.hideFlags = HideFlags.HideInHierarchy;
-// hiddenMainLoop.AddComponent<> ();
+// hiddenMainLoop.AddComponent<>();
