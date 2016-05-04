cc959f91ba487196fb54416f7c68e078dded4d52
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-05-04 14:48:40 +0000

    Add basic mouse event system.

---
+++ Unity/Defenses_immunitaires/Assets/Scripts/System/ColorSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ColorSystem : UECS.System {
+	Family _family;
+
+	public ColorSystem() {
+		_family = FamilyManager.getFamily(new AllOfTypes(typeof(MousePressedOn)));
+	}
+
+	public override void process(int currentFrame) {
+		foreach (GameObject gameObject in _family) {
+			Material material = gameObject.GetComponent<Renderer>().material;
+			material.color = (material.color == Color.black) ? Color.red : Color.black;
+		}
+	}
+}
--- Unity/Defenses_immunitaires/Assets/Scripts/System/System.cs
+++ Unity/Defenses_immunitaires/Assets/Scripts/System/System.cs
@@ -3,5 +3,14 @@
 		public bool Pause { get; set; }
 
 		public abstract void process(int currentFrame);
+
+		// ensure no more than 1 singleton by subclass
 	}
 }
+
+
+
+
+//public System () {
+//	UnityEngine.Debug.Log(this.GetType());
+//}
