329165b8ede51ca6e6e2b4668050ec2073b73484
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-05-06 14:34:45 +0000

    Change MainLoop inspector in playing mode.

--- Defenses_immunitaires/Assets/Scripts/MainLoop/MainLoopInspector.cs
+++ Defenses_immunitaires/Assets/Scripts/MainLoop/MainLoopInspector.cs
@@ -1,35 +1,89 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(MainLoop))]
 public class MainLoopInspector : Editor {
-	private SerializedProperty _systemFiles;
-	private SerializedProperty _activate;
-	private SerializedProperty _order;
-
 	private GUILayoutOption _minButtonWidth;
-	private GUIContent _addButton;
-	private GUIContent _activateButton;
+	private GUIContent _pauseButton;
 	private GUIContent _moveUpButton;
 	private GUIContent _moveDownButton;
+
+	// EDITING MODE
+	private SerializedProperty _systemFiles;
+	private SerializedProperty _pause;
+	private GUIContent _addButton;
 	private GUIContent _removeButton;
+
+	// PLAYING MODE
+	private MainLoop _target;
 
 	private void OnEnable(){
 		if (Object.FindObjectsOfType<MainLoop>().Length > 1) { // due to duplicate hotkeys & option on GO
-			DestroyImmediate(target);
+			DestroyImmediate(this.target);
 			return;
 		}
 
-		_systemFiles = serializedObject.FindProperty("_systemFiles");
-		_activate = serializedObject.FindProperty("_activate");
-		_order = serializedObject.FindProperty("_order");
-
 		_minButtonWidth = GUILayout.MinWidth(20f);
-		_addButton = new GUIContent("+", "add");
-		_activateButton = new GUIContent("", "activate");
+		_pauseButton = new GUIContent("", "pause");
 		_moveUpButton = new GUIContent("\u25B2", "move up");
 		_moveDownButton = new GUIContent("\u25BC", "move down");
-		_removeButton = new GUIContent("-", "remove");
+
+		if (!Application.isPlaying) {
+			_systemFiles = serializedObject.FindProperty("_systemFiles");
+			_pause = serializedObject.FindProperty("_pause");
+			_addButton = new GUIContent("+", "add");
+			_removeButton = new GUIContent("-", "remove");
+		} else {
+			_target = (MainLoop)this.target;
+		}
+	}
+
+	public override void OnInspectorGUI(){
+		if (Application.isPlaying) {
+			this.playingModeInspector();
+		} else {
+			serializedObject.Update();
+			this.editingModeInspector();
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+
+	private void playingModeInspector(){		
+		List<UECS.System> systems = _target._systems;
+		int systemsCount = systems.Count;
+
+		for(int i = 0; i < systemsCount; ++i) {
+			UECS.System system = systems[i];
+
+			EditorGUILayout.BeginHorizontal();
+
+			EditorGUILayout.LabelField(system.GetType().ToString());
+
+			Color baseColor = GUI.color;
+			GUI.color = (system.Pause == true) ? Color.red : Color.green;
+			if(GUILayout.Button(_pauseButton, _minButtonWidth))
+				system.Pause = !system.Pause;
+			GUI.color = baseColor;
+
+			if (GUILayout.Button (_moveUpButton, _minButtonWidth)) {
+				if (i != 0) {
+					UECS.System acc = systems [i - 1];
+					systems[i - 1] = systems [i];
+					systems[i] = acc;
+				}
+			}
+
+			if (GUILayout.Button (_moveDownButton, _minButtonWidth)) {
+				if (i != systemsCount - 1) {
+					UECS.System acc = systems [i + 1];
+					systems [i + 1] = systems [i];
+					systems [i] = acc;
+				}
+			}
+
+			EditorGUILayout.EndHorizontal();
+		}
 	}
 
 	private void editingModeInspector() {
@@ -41,24 +95,28 @@ public class MainLoopInspector : Editor {
 			if (systemFile.objectReferenceValue != script && script != null && script.GetClass().IsSubclassOf(typeof(UECS.System)))
 				systemFile.objectReferenceValue = script;
 			
-			this.showActivateButton(_activate.GetArrayElementAtIndex(i));
+			Color baseColor = GUI.color;
+			SerializedProperty pause = _pause.GetArrayElementAtIndex(i);
+			GUI.color = (pause.boolValue == true) ? Color.red : Color.green;
+			if(GUILayout.Button(_pauseButton, _minButtonWidth))
+				pause.boolValue = !pause.boolValue;
+			GUI.color = baseColor;
+
 			if (GUILayout.Button (_moveUpButton, _minButtonWidth)) {
 				_systemFiles.MoveArrayElement (i, i - 1);
-				_activate.MoveArrayElement (i, i - 1);
+				_pause.MoveArrayElement (i, i - 1);
 			}
+
 			if (GUILayout.Button (_moveDownButton, _minButtonWidth)) {
 				_systemFiles.MoveArrayElement (i, i + 1);
-				_activate.MoveArrayElement (i, i + 1);
+				_pause.MoveArrayElement (i, i + 1);
 			}
+
 			if(GUILayout.Button (_removeButton, _minButtonWidth)) {
 				_systemFiles.GetArrayElementAtIndex(i).objectReferenceValue = null;
 				_systemFiles.DeleteArrayElementAtIndex(i);
 
-				_activate.DeleteArrayElementAtIndex(i);
-
-				_order.DeleteArrayElementAtIndex(i);
-				for(int j = i; j < _order.arraySize; ++j)
-					_order.GetArrayElementAtIndex (j).intValue = j;
+				_pause.DeleteArrayElementAtIndex(i);
 			}
 
 			EditorGUILayout.EndHorizontal();
@@ -69,50 +127,8 @@ public class MainLoopInspector : Editor {
 			_systemFiles.InsertArrayElementAtIndex(index);
 			_systemFiles.GetArrayElementAtIndex(index).objectReferenceValue = null;
 
-			_activate.InsertArrayElementAtIndex(index);
-			_activate.GetArrayElementAtIndex(index).boolValue = false;
-
-			_order.InsertArrayElementAtIndex(index);
-			_order.GetArrayElementAtIndex (index).intValue = index;
+			_pause.InsertArrayElementAtIndex(index);
+			_pause.GetArrayElementAtIndex(index).boolValue = true;
 		}
-	}
-
-	private void playingModeInspector(){
-		for (int i = 0; i < _order.arraySize; ++i) {
-			EditorGUILayout.BeginHorizontal ();
-
-			int index = _order.GetArrayElementAtIndex(i).intValue;
-			SerializedProperty systemFile = _systemFiles.GetArrayElementAtIndex(index);
-			EditorGUILayout.ObjectField (systemFile.objectReferenceValue, typeof(MonoScript), false);
-
-			this.showActivateButton(_activate.GetArrayElementAtIndex(index));
-			if (GUILayout.Button (_moveUpButton, _minButtonWidth))
-				_order.MoveArrayElement(i, i - 1);
-			if (GUILayout.Button (_moveDownButton, _minButtonWidth))
-				_order.MoveArrayElement (i, i + 1);
-
-			EditorGUILayout.EndHorizontal();
-		}
-	}
-
-	private void showActivateButton(SerializedProperty elementIsActivatedProperty){
-		Color baseColor = GUI.color;
-
-		GUI.color = (elementIsActivatedProperty.boolValue == true) ? Color.green : Color.red;
-		if(GUILayout.Button(_activateButton, _minButtonWidth))
-			elementIsActivatedProperty.boolValue = !elementIsActivatedProperty.boolValue;
-
-		GUI.color = baseColor;
-	}
-
-	public override void OnInspectorGUI(){
-		serializedObject.Update();
-
-		if (!Application.isPlaying)
-			this.editingModeInspector();
-		else
-			this.playingModeInspector();
-
-		serializedObject.ApplyModifiedProperties();
 	}
 }
