2f95903426e0c562ebc3cc1cd29f8ac3c2038165
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-06-02 13:26:03 +0000

    Add Trigger dll.

---
+++ FYFY_plugins/Trigger/src/TriggerSensitive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+// Ecrire dans la doc : composant ajoute et supprime automatiquement. NE PAS Y TOUCHER MANUELLEMENT SINON ON PEUT PAS GARANTIR LES COMPORTEMENTS!!!!!!!!!
+// OnTrigger apres le fixedUpdate (apres internal physics update)
+namespace FYFY_plugins.Trigger {
+	public abstract class TriggerSensitive : MonoBehaviour {
+		protected int _gameObjectId;
+		protected List<GameObject> _others;
+		internal ReadOnlyCollection<GameObject> _othersReadOnly;
+
+		protected void Awake() {
+			_gameObjectId = this.gameObject.GetInstanceID();
+			_others = new List<GameObject>();
+			_othersReadOnly = new ReadOnlyCollection<GameObject>(_others);
+		}
+	}
+}
