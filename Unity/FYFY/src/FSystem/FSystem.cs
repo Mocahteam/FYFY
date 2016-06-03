483947294a8ea9cb8db6324f48e819b24b1fb0bf
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-06-03 13:31:38 +0000

    Fix minor problem (setter of System Pause wasn't call when pause in editor == false due to default bool value // now Pause is a bool? == bool which can be nullable)

--- Unity/FYFY/src/FSystem/FSystem.cs
+++ Unity/FYFY/src/FSystem/FSystem.cs
@@ -1,18 +1,18 @@
 ﻿namespace FYFY {
 	public abstract class FSystem {
-		private bool _pause;
+		private bool? _pause; // null for initialization because bool default == 0 -> donc sinon le setter n'est appele a linit quand de la cas on set a true (true != false)
 
 		public bool Pause {
-			get { 
-				return _pause;
+			get {
+				return (_pause == null) ? false : (bool)_pause;
 			}
 			set {
-				if (value != _pause) {
+				if(value != _pause) {
 					_pause = value;
-					if (value == false)
-						this.onPause(UnityEngine.Time.frameCount);
-					else
+					if(value == false)
 						this.onResume(UnityEngine.Time.frameCount);
+					else
+						this.onPause(UnityEngine.Time.frameCount);
 				}
 			}
 		}
