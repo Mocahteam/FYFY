66a6e46cec01d7a33983ae85a967c36489b7945c
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-06-15 15:42:19 +0000

    Simplify virus polygon collider && show infected property in inspector.

--- Unity/FYFY_projects/Defenses_immunitaires_FYFY_2D/Assets/Scripts/Components/Infected.cs
+++ Unity/FYFY_projects/Defenses_immunitaires_FYFY_2D/Assets/Scripts/Components/Infected.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
 
 public class Infected : MonoBehaviour {
-	[HideInInspector]
 	public VirusProperties _virusProperties;
-	[HideInInspector]
 	public int _virusNumberToCreate = 0;
-	[HideInInspector]
 	public float _virusProductionProgress = 0f;
 }
