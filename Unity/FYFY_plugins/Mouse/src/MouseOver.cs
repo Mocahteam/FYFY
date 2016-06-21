738c68172143f99d7233212e287933a0078be1cb
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-06-21 12:19:23 +0000

    Add documentation comments && allow the documentation generation with csproj.

--- Unity/FYFY_plugins/Mouse/src/MouseOver.cs
+++ Unity/FYFY_plugins/Mouse/src/MouseOver.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 
 namespace FYFY_plugins.Mouse {
+	/// <summary>
+	/// 	Component specifying that the mouse is over the <c>GameObject</c>.
+	/// </summary>
+	/// <remarks>
+	/// 	Automatically added or removed by the relative <see cref="FYFY_plugins.Mouse.MouseSensitive">component</see>.
+	/// </remarks>
 	[DisallowMultipleComponent]
 	[RequireComponent(typeof(MouseSensitive))]
 	[AddComponentMenu("")]
--- Unity/FYFY_plugins/Mouse/src/MouseSensitive.cs
+++ Unity/FYFY_plugins/Mouse/src/MouseSensitive.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 
 namespace FYFY_plugins.Mouse {
+	/// <summary>
+	/// 	Component allowing <c>GameObject</c> to be sensitive to the mouse.
+	/// 	Add automatically a <see cref="FYFY_plugins.Mouse.MouseOver">component</see> when the mouse points the <c>GameObject</c>.
+	/// </summary>
+	/// <remarks>
+	/// 	<para>Overlay of the <c>Unity Mouse System</c>.</para>
+	/// 	<para>It's a tricky component with non FYFY conventional design.</para>
+	/// 	<para>It uses Unity logic inside so it can't be considered like a real FYFY component.</para>
+	/// </remarks>
 	[DisallowMultipleComponent]
 	public class MouseSensitive : MonoBehaviour {
+		/// <summary>
+		/// 	Called when the mouse enters the <c>GameObject</c>.
+		/// </summary>
+		/// <remarks>
+		/// 	Add a <see cref="FYFY_plugins.Mouse.MouseOver">component</see> to the <c>GameObject</c>.
+		/// </remarks>
 		private void OnMouseEnter() {
 			FYFY.GameObjectManager.addComponent<MouseOver>(this.gameObject);
 		}
+
+		/// <summary>
+		/// 	Called when the mouse is not any longer over the <c>GameObject</c>.
+		/// </summary>
+		/// <remarks>
+		/// 	Remove the <see cref="FYFY_plugins.Mouse.MouseOver">component</see> attached to the <c>GameObject</c>.
+		/// </remarks>
 		private void OnMouseExit() {
 			FYFY.GameObjectManager.removeComponent<MouseOver>(this.gameObject);
 		}
