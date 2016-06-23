8c80deda62862c312e1dfa628aa562f198df5c6a
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-06-23 10:08:57 +0000

    Specification in comments on which GameObject MouseSensitive can work (equal to OnMouseEnter dependencies).

--- Unity/FYFY_plugins/Mouse/src/MouseSensitive.cs
+++ Unity/FYFY_plugins/Mouse/src/MouseSensitive.cs
@@ -6,6 +6,10 @@ namespace FYFY_plugins.Mouse {
 	/// 	Add automatically a <see cref="FYFY_plugins.Mouse.MouseOver">component</see> when the mouse points the <c>GameObject</c>.
 	/// </summary>
 	/// <remarks>
+	/// 	<para>
+	///			This component works only on objects that aren't on Ignore Raycast layer 
+	///			and which have a collider marked as Trigger (with <c>Physics.queriesHitTriggers</c> true).
+	///		</para>
 	/// 	<para>Overlay of the <c>Unity Mouse System</c>.</para>
 	/// 	<para>It's a tricky component with non FYFY conventional design.</para>
 	/// 	<para>It uses Unity logic inside so it can't be considered like a real FYFY component.</para>
