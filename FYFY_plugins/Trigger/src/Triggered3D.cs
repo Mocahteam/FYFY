5d59709547fa143c47fdc9e39c373116e745427a
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-07-07 11:17:13 +0000

    Add not definitive documentation.

--- FYFY_plugins/Trigger/src/Triggered3D.cs
+++ FYFY_plugins/Trigger/src/Triggered3D.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 
 namespace FYFY_plugins.Trigger {
+	/// <summary>
+	/// 	Component specifying that the <c>GameObject</c>(the source) is in contact with at least one other <c>GameObject</c>(the target).
+	/// </summary>
+	/// <remarks>
+	/// 	<para>Automatically added, updated or removed by the relative <see cref="FYFY_plugins.Trigger.TriggerSensitive3D">component</see>.</para>
+	/// 	<para>DO NOT TOUCH MANUALLY OTHERWISE WE CAN'T GUARANTEE THE COMPORTMENT.</para>
+	/// </remarks>
 	[DisallowMultipleComponent]
 	[AddComponentMenu("")]
 	[RequireComponent(typeof(TriggerSensitive3D))]
 	public class Triggered3D : Triggered {
+		//* <summary>
+		//* 	Called after the <c>component</c> was created to initialized the pointer on the relative <see cref="FYFY_plugins.Trigger.TriggerSensitive3D._targets"/>.
+		//* </summary>
+		//* <remarks>
+		//* 	<para>Callback automatically called by Unity.</para>
+		//* </remarks>
 		private void Awake() {
 			TriggerSensitive3D ts3D = this.gameObject.GetComponent<TriggerSensitive3D>();
 			_targets = ts3D._targets;
