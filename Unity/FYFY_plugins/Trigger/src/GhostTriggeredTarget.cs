e677fe5e59d66bdb99a68df8d50cc1722e152ac5
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-07-07 11:17:13 +0000

    Add not definitive documentation.

--- Unity/FYFY_plugins/Trigger/src/GhostTriggeredTarget.cs
+++ Unity/FYFY_plugins/Trigger/src/GhostTriggeredTarget.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 
 namespace FYFY_plugins.Trigger {
+	//* <summary>
+	//* 	Ghost triggered target.
+	//* </summary>
+
+	// TESTER EN LA METTANT INTERNAL ????????????????
+
 	[AddComponentMenu("")]
 	[HideInInspector]
 	public class GhostTriggeredTarget : MonoBehaviour {
+		/// <summary>
+		/// The trigger sensitive source.
+		/// </summary>
 		public TriggerSensitive _triggerSensitiveSource;
 
+		/// <summary>
+		/// Raises the destroy event.
+		/// </summary>
 		private void OnDestroy(){ // check if you have to destroy the corresponding Triggered2D or Triggered3D
+			Debug.Log("YEAH");
+
 			if(_triggerSensitiveSource == null || _triggerSensitiveSource._destroying == true) { // triggerSensitive destroyed before gtt
 				return;
 			}
--- Unity/FYFY_plugins/Trigger/src/Triggered2D.cs
+++ Unity/FYFY_plugins/Trigger/src/Triggered2D.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 
 namespace FYFY_plugins.Trigger {
+	/// <summary>
+	/// 	Component specifying that the <c>GameObject</c>(the source) is in contact with at least one other <c>GameObject</c>(the target).
+	/// </summary>
+	/// <remarks>
+	/// 	<para>Automatically added, updated or removed by the relative <see cref="FYFY_plugins.Trigger.TriggerSensitive2D">component</see>.</para>
+	/// 	<para>DO NOT TOUCH MANUALLY OTHERWISE WE CAN'T GUARANTEE THE COMPORTMENT.</para>
+	/// </remarks>
 	[DisallowMultipleComponent]
 	[AddComponentMenu("")]
 	[RequireComponent(typeof(TriggerSensitive2D))]
 	public class Triggered2D : Triggered {
+		//* <summary>
+		//* 	Called after the <c>component</c> was created to initialized the pointer on the relative <see cref="FYFY_plugins.Trigger.TriggerSensitive2D._targets"/>.
+		//* </summary>
+		//* <remarks>
+		//* 	<para>Callback automatically called by Unity.</para>
+		//* </remarks>
 		private void Awake() {
 			TriggerSensitive2D ts2D = this.gameObject.GetComponent<TriggerSensitive2D>();
 			_targets = ts2D._targets;
