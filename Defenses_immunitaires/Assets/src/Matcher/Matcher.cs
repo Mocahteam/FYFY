5c94acc96991e9da13047a8c7cc9d9751d4ebefc
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-08-17 13:20:46 +0000

    Remove test project repository.

--- Defenses_immunitaires/Assets/src/Matcher/Matcher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FYFY {
-	public abstract class Matcher {
-		internal string _descriptor;
-
-		public string Descriptor { get { return _descriptor; } }
-
-		internal abstract bool matches(GameObjectWrapper gameObjectWrapper);
-	}
-}
