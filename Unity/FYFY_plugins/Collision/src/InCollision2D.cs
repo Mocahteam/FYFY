64d947efe7ce5a45ad0ad957bacd40f21568c602
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-09-27 16:03:55 +0000

    Only to save current work (no test done).

---
+++ Unity/FYFY_plugins/Collision/src/InCollision2D.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace FYFY_plugins.CollisionDetection {
+	[DisallowMultipleComponent]
+	[AddComponentMenu("")]
+	[RequireComponent(typeof(CollisionSensitive2D))]
+	public class InCollision2D : MonoBehaviour {
+		private CollisionSensitive2D _collisionSensitive;
+
+		private void Awake() {
+			_collisionSensitive = this.gameObject.GetComponent<CollisionSensitive2D>();
+		}
+
+		public GameObject[] Targets {
+			get {
+				return _collisionSensitive._collisions.Keys.ToArray();
+			}
+		}
+
+		public Collision2D[] Collisions {
+			get {
+				return _collisionSensitive._collisions.Values.ToArray();
+			}
+		}
+	}
+}
