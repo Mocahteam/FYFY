0c59498ce5827f19fa9bed50b13f9761d455c4e4
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-09-27 16:03:55 +0000

    Only to save current work (no test done).

---
+++ FYFY_plugins/Collision/src/CollisionSensitive2D.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using FYFY;
+
+namespace FYFY_plugins.CollisionDetection {
+	[DisallowMultipleComponent]
+	public abstract class CollisionSensitive2D : MonoBehaviour {
+		internal Dictionary<GameObject, Collision2D> _collisions = new Dictionary<GameObject, Collision2D>();
+		internal Dictionary<GameObject, CollisionSensitive2DTarget> _components = new Dictionary<GameObject, CollisionSensitive2DTarget>();
+
+		internal bool _destroying = false;
+		internal bool _inCollision = false;
+
+		private void OnCollisionEnter2D(Collision2D coll) {
+			GameObject target = coll.gameObject;
+			CollisionSensitive2DTarget cst = target.gameObject.AddComponent<CollisionSensitive2DTarget>();
+			cst._source = this;
+
+			_collisions.Add(target, coll);
+			_components.Add(target, cst);
+
+			if(_inCollision == false) {
+				GameObjectManager.addComponent<InCollision2D>(this.gameObject);
+				_inCollision = true;
+			}
+		}
+
+		private void OnCollisionExit2D(Collision2D coll) {
+			GameObject target = coll.gameObject;
+			CollisionSensitive2DTarget cst = _components[target];
+			Component.Destroy(cst);
+
+			_collisions.Remove(target);
+			_components.Remove(target);
+
+			if(_collisions.Count == 0) {
+				GameObjectManager.removeComponent<InCollision2D>(this.gameObject);
+				_inCollision = false;
+			}
+		}
+
+		private void OnDestroy() {
+			_destroying = true;
+
+			foreach(CollisionSensitive2DTarget cst in _components.Values) {
+				Object.Destroy(cst);
+			}
+		}
+	}
+}
---
+++ FYFY_plugins/Collision/src/InCollision2DTarget.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using FYFY;
+
+namespace FYFY_plugins.CollisionDetection {
+	[AddComponentMenu("")]
+	[HideInInspector]
+	public class CollisionSensitive2DTarget : MonoBehaviour { // one by collision so multiple possible
+		public CollisionSensitive2D _source;
+
+		private void OnDestroy(){
+			if(_source == null || _source._destroying == true) {
+				return;
+			}
+			
+			_source._collisions.Remove(this.gameObject);
+			_source._components.Remove(this.gameObject);
+
+			if (_source._collisions.Count == 0) {
+//				foreach(FYFY.IGameObjectManagerAction action in FYFY.GameObjectManager._delayedActions) {
+//					if(action.GetType() == typeof(DestroyGameObject)) {
+//						if(((DestroyGameObject)action)._gameObject == _source.gameObject)
+//							return;
+//					}
+//				}
+
+				GameObjectManager.removeComponent<InCollision2D>(_source.gameObject);
+				_source._inCollision = false;
+			}
+		}
+	}
+}
