dfa9d38c17a26b2aec9f920f81807cbbb0015b2b
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-10-01 14:52:22 +0000

    Remove CollisionSensitive2D._destroying and begin to add documentation. (In CollisionSensitive2D.OnDestroy, we use Destroy and not DestroyImmediate so _destroying is useless)

--- Unity/FYFY_plugins/CollisionManager/src/CollisionSensitive2DTarget.cs
+++ Unity/FYFY_plugins/CollisionManager/src/CollisionSensitive2DTarget.cs
@@ -2,28 +2,45 @@
 using FYFY;
 
 namespace FYFY_plugins.CollisionManager {
+	/// <summary></summary>
+
+	// One by collision so multiple possible
+
 	[AddComponentMenu("")]
-	//[HideInInspector]
-	public class CollisionSensitive2DTarget : MonoBehaviour { // one by collision so multiple possible
+	[HideInInspector]
+	public class CollisionSensitive2DTarget : MonoBehaviour {
+		/// <summary></summary>
+
+		// Sets in the source CollisionSensitive2D.OnCollisionEnter.
 		public CollisionSensitive2D _source;
 
+		// Unsubscribes the target (GameObject relatives to this component) from the source if necessary.
 		private void OnDestroy(){
-			if(_source == null || _source._destroying == true) {
+			if(_source == null) {
 				return;
 			}
 			
 			_source._collisions.Remove(this.gameObject);
 			_source._components.Remove(this.gameObject);
 
+			// Remove source InCollision2D component if necessary.
 			if(_source._collisions.Count == 0) {
-				foreach(FYFY.IGameObjectManagerAction action in FYFY.GameObjectManager._delayedActions) {
-					if(action.GetType() == typeof(DestroyGameObject)) {
-						if(((DestroyGameObject)action)._gameObject == _source.gameObject) {
-							return;
-						}
-					}
-				}
+				// If there is a DestroyGameObject action on the source, nothing to do.
+//				foreach(FYFY.IGameObjectManagerAction action in FYFY.GameObjectManager._delayedActions) {
+//					if(action.GetType() == typeof(DestroyGameObject)) {
+//						if(((DestroyGameObject)action)._gameObject == _source.gameObject) {
+//							return;
+//						}
+//					}
+//				}
 
+				//
+				// Two important origins of this OnDestroy calling:
+				// 		- The source CollisionSensitive2D.OnCollisionExit2D,
+				// 		- GameObjectManager.Destroy with the relative GameObject to this component.
+				// ->
+				// ... ???
+				//
 				GameObjectManager.removeComponent<InCollision2D>(_source.gameObject);
 				_source._inCollision = false;
 			}
