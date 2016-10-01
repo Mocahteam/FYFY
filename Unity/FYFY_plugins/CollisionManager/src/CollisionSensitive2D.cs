22868f3df39d61db553e63d6aada523b8396020e
salvatoret <salvatoret@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-10-01 16:54:54 +0000

    Add documentation.

--- Unity/FYFY_plugins/CollisionManager/src/CollisionSensitive2D.cs
+++ Unity/FYFY_plugins/CollisionManager/src/CollisionSensitive2D.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using FYFY;
 
 namespace FYFY_plugins.CollisionManager {
+	/// <summary>
+	/// 	Component allowing GameObject to be noticed when it is in contact with another GameObject.
+	/// </summary>
 	[DisallowMultipleComponent]
 	public class CollisionSensitive2D : MonoBehaviour {
+		// Target / Collision informations
 		internal Dictionary<GameObject, Collision2D> _collisions = new Dictionary<GameObject, Collision2D>();
+		// Target / CollisionSensitive2DTarget corresponding (ie link between this gameobject and the target)
 		internal Dictionary<GameObject, CollisionSensitive2DTarget> _components = new Dictionary<GameObject, CollisionSensitive2DTarget>();
 
 		internal bool _inCollision = false;
 
 		private void OnCollisionEnter2D(Collision2D coll) {
 			GameObject target = coll.gameObject;
+			// We don't want that FYFY treates CollisionSensitive2DTarget component.
 			CollisionSensitive2DTarget cst = target.gameObject.AddComponent<CollisionSensitive2DTarget>();
 			cst._source = this;
 
@@ -19,7 +25,9 @@ namespace FYFY_plugins.CollisionManager {
 			_components.Add(target, cst);
 
 			if(_inCollision == false) {
-				GameObjectManager.addComponent<InCollision2D>(this.gameObject); // add at next preprocess so immediatly after ! (on aurait pu lajouter direct mais on doit signaler a FYFY lajout pr les familles)
+				// This action will be treated immediatly after all the collisions
+				// (next preprocess operation is done before the next simulation step).
+				GameObjectManager.addComponent<InCollision2D>(this.gameObject);
 				_inCollision = true;
 			}
 		}
@@ -29,6 +37,7 @@ namespace FYFY_plugins.CollisionManager {
 			_collisions[target] = coll;
 		}
 
+		// Not fired when this GameObject or the target is destroyed.
 		private void OnCollisionExit2D(Collision2D coll) {
 			GameObject target = coll.gameObject;
 			CollisionSensitive2DTarget cst = _components[target];
--- Unity/FYFY_plugins/CollisionManager/src/InCollision2D.cs
+++ Unity/FYFY_plugins/CollisionManager/src/InCollision2D.cs
@@ -2,6 +2,13 @@
 using System.Linq;
 
 namespace FYFY_plugins.CollisionManager {
+	/// <summary>
+	/// 	Component specifying that the GameObject is in contact with at least one other GameObject.
+	/// </summary>
+	/// <remarks>
+	/// 	<para>! AUTOMATICALLY ADDED, UPDATED OR REMOVED !</para>
+	/// 	<para>! DO NOT TOUCH MANUALLY OTHERWISE WE CAN'T GUARANTEE THE COMPORTMENT !</para>
+	/// </remarks>
 	[DisallowMultipleComponent]
 	[AddComponentMenu("")]
 	[RequireComponent(typeof(CollisionSensitive2D))]
@@ -12,12 +19,18 @@ namespace FYFY_plugins.CollisionManager {
 			_collisionSensitive = this.gameObject.GetComponent<CollisionSensitive2D>();
 		}
 
+		/// <summary>
+		/// 	Gets all the GameObjects in contact with this GameObject.
+		/// </summary>
 		public GameObject[] Targets {
 			get {
 				return _collisionSensitive._collisions.Keys.ToArray();
 			}
 		}
 
+		/// <summary>
+		/// 	Gets all the collisions informations involving this GameObject.
+		/// </summary>
 		public Collision2D[] Collisions {
 			get {
 				return _collisionSensitive._collisions.Values.ToArray();
