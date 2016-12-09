e6ec5e50da2c3dbf587e7b3f4925fb84474146ba
muratetm <muratetm@24b8b584-3c39-4f72-97e8-4f9f36303fd6>
2016-12-09 18:37:02 +0000

    Add MLI - MainLoopInspector - plugin (need to set "Select platforms for plugin" to "Editor" in Unity at the import).

---
+++ Unity/FYFY_plugins/MLI/src/MainLoopCreationMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace FYFY_plugins.MLI {
+	/// <summary>
+	/// 	Class automatically loaded by Unity Editor to create the FYFY menu.
+	/// </summary>
+	public static class MainLoopCreationMenu {
+		[MenuItem("FYFY/Create Main Loop %m")]
+		private static void createMainLoop() {
+			string name = "Main_Loop";
+			GameObject mainLoop = GameObject.Find(name);
+			if(mainLoop == null) {
+				mainLoop = new GameObject(name);
+				mainLoop.isStatic = true;
+			}
+			
+			mainLoop.AddComponent<FYFY.MainLoop>();
+		}
+
+		[MenuItem("FYFY/Create Main Loop %m", true)]
+		private static bool activeMenu() {
+			return Object.FindObjectOfType<FYFY.MainLoop>() == null && !Application.isPlaying;
+		}
+	}
+}
