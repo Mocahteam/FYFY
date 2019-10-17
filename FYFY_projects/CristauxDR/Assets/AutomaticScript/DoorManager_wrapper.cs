02058f8d9449ed2f2bf818deedbac93fab54e14b
Muratet <mathieu.muratet@gmail.com>
2019-10-17 16:13:43 +0200

    Update libraries

---
+++ FYFY_projects/CristauxDR/Assets/AutomaticScript/DoorManager_wrapper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using FYFY;
+
+[ExecuteInEditMode]
+public class DoorManager_wrapper : MonoBehaviour
+{
+	private void Start()
+	{
+		this.hideFlags = HideFlags.HideInInspector; // Hide this component in Inspector
+	}
+
+}
